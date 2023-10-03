2b71ac0c32a5ad88d3dcd94c58853abcb4c7f722
dreamlike_ocean <dreamlike.vertx@gmail.com>
2023-10-03 18:05:47 +0800

    1.fix 编译失败的问题

---
+++ src/main/java/org/example/PolyplotDemo.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
+
+public class PolyplotDemo {
+    public static void main(String[] args) {
+        try (Context context = Context.newBuilder("js").option("js.ecmascript-version", "2020").build()) {
+            Value jsFunction = context.eval("js", "" +
+                    "(function myJavaScriptFunction(parameter) {         \n" +
+                    "    console.log('[JS] Hello, ' + parameter + '!');  \n" +
+                    "    return parameter.toUpperCase();                 \n" +
+                    "})                                                  \n");
+
+            Value result = jsFunction.execute("JavaScript");
+            if (result.isString()) {
+                System.out.println("[Java] result: " + result.asString());
+            } else {
+                System.out.println("[Java] unexpected result type returned from JavaScript");
+            }
+        }
+
+    }
+}
